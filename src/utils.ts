b35bc7c0d80ad487af5323977e56f732e88b25a9
NextKolya <phelixgluy@gmail.com>
2026-01-21 20:00:39 +0600

    add utils

---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+import type { Benchmarks } from './types';
+
+/**
+ * #### just returns `performance.now()`
+ *
+ * @returns {number} milliseconds timestamp
+ */
+export const now = () => performance.now();
+
+/**
+ *
+ *
+ *
+ * #### Runs a loop on the specified `iterations` argument to warm the cpu up
+ *
+ *
+ *
+ * @param {number} iterations quantity of loop iterations
+ */
+export const warmup = (iterations: number = 1_000_000) => {
+    for (let i = 0; i < iterations; i++) {}
+};
+
+/**
+ * #### intializes a `Map` with type `Benchmarks`
+ *
+ * @returs initialized benchmarks
+ */
+export const initBenches = (): Benchmarks => {
+    return new Map();
+};
